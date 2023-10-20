aee75699ba1b8bc016e95bf37c3558bfd1f5deed
Evgenia-cyber <kurineva85@mail.ru>
2023-10-20 23:23:04 +0300

    refactor js code

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
-// плавный переход к секции products при клике по кнопке "Смотреть меню"
+const smoothScrollTo = (elementId) => {
+  document.getElementById(elementId).scrollIntoView({ behavior: "smooth" });
+};
+
+// плавный переход к секции с id=products при клике по кнопке "Смотреть меню"
 document.getElementById("main-action-button").onclick = function () {
-  // указываем id элемента, к которому будет совершен переход
-  document.getElementById("products").scrollIntoView({ behavior: "smooth" });
+  // передаем id элемента, к которому будет совершен переход
+  smoothScrollTo("products");
 };
 
 // находим все ссылки, которые находятся в блоках menu-item
@@ -10,34 +14,30 @@ let links = document.querySelectorAll(".menu-item > a");
 // плавный переход к нужной секции при клике по соответствующему пункту меню
 for (let i = 0; i < links.length; i++) {
   links[i].onclick = function () {
-    document
-      // значение data-link должно быть равно значению id элемента, к которому будет совершен переход
-      .getElementById(links[i].getAttribute("data-link"))
-      .scrollIntoView({ behavior: "smooth" });
+    // значение data-link должно быть равно значению id элемента, к которому будет совершен переход
+    smoothScrollTo(links[i].getAttribute("data-link"));
   };
 }
 
-// находим все кнопки Заказать
+// находим все кнопки "Заказать"
 let buttons = document.getElementsByClassName("products-button");
 
 // плавный переход к форме для оформления заказа при клике по любой кнопке "Заказать"
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].onclick = function () {
-    document.getElementById("order").scrollIntoView({ behavior: "smooth" });
+    smoothScrollTo("order");
   };
 }
 
 // валидация формы
-let burger = document.getElementById("burger");
-let name = document.getElementById("name");
-let phone = document.getElementById("phone");
+let inputs = Array.from(document.getElementsByTagName("input"));
 
 document.getElementById("order-action").onclick = function (event) {
   event.preventDefault();
 
   let hasError = false;
 
-  [burger, name, phone].forEach((item) => {
+  inputs.forEach((item) => {
     //  если какое-то поле формы не заполнено, то меняем цвет рамки на красный
     if (!item.value) {
       item.parentElement.style.background = "red";
@@ -50,7 +50,7 @@ document.getElementById("order-action").onclick = function (event) {
 
   // если все поля формы заполнены, очищаем поля и выводим alert
   if (!hasError) {
-    [burger, name, phone].forEach((item) => {
+    inputs.forEach((item) => {
       item.value = "";
     });
 
